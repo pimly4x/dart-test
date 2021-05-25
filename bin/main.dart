5ec6f8ce11c3e2d98727956364c81285dfc80756
Yaw Sarfo Yeboah <yawye@stlghana.com>
2021-05-25 16:54:48 +0000

    test

---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+main(){
+print("===============");
+print("Please enter first number");
+
+String Full_name = stdin.readLineSync();
+print('Hello : $Full_name');
+
+print("=========================");
+
+
+}
